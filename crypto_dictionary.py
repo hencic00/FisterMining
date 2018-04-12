00bb93e0c257341e10484ece3b77a37b182a14fa
Dejan Rupnik <dejan.rupnik.1@gmail.com>
2018-04-12 21:51:37 +0200

    izdelava kripto slovarja, katerega rabimo za razpoznavo crypto v tweetih

---
+++ crypto_dictionary.py
@@ -0,0 +1,37 @@
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+import pickle
+url = "https://api.coinmarketcap.com/v1/ticker/?limit=0"
+headers = {"Accept": "application/json"}
+data = '{}'
+dict  = {}
+
+def save_obj(obj, name):
+    with open('dictionaries/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open('dictionaries/'+ name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+#get cryptocurrency names and symbols
+response = requests.get(url,data = data)
+if(response.ok):
+    jData = json.loads(response.content)
+    print("Crypto market contains {0} cryptocurrencies".format(len(jData)))
+    print("\n")
+    
+    for crypto in jData:
+        dict[crypto['name'].encode('utf-8')] = crypto['symbol'].encode('utf-8')
+
+
+#save to file
+save_obj(dict, "cryptos")
+
+#load from file
+dict = load_obj("cryptos")
+for c in dict:
+    print (c)
+
