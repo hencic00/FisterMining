02c57bec3f5b5e064a3eaad11e4c0f769a1942d1
Dejan Rupnik <dejan.rupnik.1@gmail.com>
2018-04-19 23:40:51 +0200

    clasifikacija tweetov

--- crypto_count.py
+++ crypto_count.py
@@ -3,11 +3,22 @@ import os
 import codecs
 from collections import namedtuple
 from crypto_dictionary import load_obj
+import re
 
 dirname = os.path.dirname(__file__)
 relative_path = "tweets\979053718241918976_978993246129946624_20000.json"
 filename = os.path.join(dirname, relative_path)
 
+class Tweet:
+    cryptos = []
+    sentiment_compoundSum = 0
+
+
+#load from file
+dict = load_obj("cryptos")  # crypto names/symbols dictionary
+""" for c in dict:
+    print (c)
+    print dict[c] """
 
 data = []
 with codecs.open(filename,'rU','utf-8') as f:
@@ -15,13 +26,19 @@ with codecs.open(filename,'rU','utf-8') as f:
         # Parse JSON into an object with attributes corresponding to dict keys.
         x = json.loads(line)
         data.append(x)
-        
+
+analysis = []   
 for tweet in data:
-    tweet['text'].encode('utf-8').lower()
+    tweet_text = tweet['text'].encode('utf-8').lower()
+    wordList = re.sub("[^\w]", " ",  tweet_text).split()  
+    instance = Tweet()
+
+    for word in wordList:
+        if word in dict:
+            instance.cryptos.append(word)
+            #TODO: dodaj sentiment analysis
+
+    analysis.append(instance) #TODO: preveri, kako deluje obseg spremenljivk v Pythonu!
 
 
-#load from file
-#dict = load_obj("cryptos")
-#for c in dict:
-#    print (c)
-#    print dict[c]
+print analysis[0].cryptos
